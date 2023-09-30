3029cb575a1dc582633b8f0cedd66001fdba43fb
Christian O. Arana Navas <COANelmejor@gmail.com>
2023-09-30 00:20:44 -0600

    type declaration without value assign

--- 02_var_let.playground/Contents.swift
+++ 02_var_let.playground/Contents.swift
@@ -6,3 +6,12 @@ var currentLoginAttempt = 0
 
 // You can declare various on one line
 var x = 0.0, y = 0.0, z = 0.0
+
+// You can declare a variable without a value, you must provide a type annotation
+var welcomeMessage : String
+
+// So you can assign a value later
+welcomeMessage = "Hello, How are you?"
+
+// You can declare a variable and assign a value in one line
+var red, green, blue : Double
